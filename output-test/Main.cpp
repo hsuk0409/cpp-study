05eb61565e6a70a13d867d48932d96e2cf286f41
earstone <hsuk0409@gmail.com>
2023-04-28 17:51:18 +0900

    output 테스트

---
+++ output-test/Main.cpp
@@ -0,0 +1,14 @@
+#include "MyOutput.h"
+#include "MySwap.h"
+
+using namespace samples;
+
+int main()
+{
+	//PrintMyString();
+
+	SwapExample();
+
+
+	return 0;
+}
---
+++ output-test/MyOutput.cpp
@@ -0,0 +1,13 @@
+#include <iomanip>
+#include <iostream>
+#include "MyOutput.h"
+
+using namespace std;
+
+namespace samples
+{
+	void PrintMyString()
+	{
+		cout << setfill('*') << setw(10) << "°°Àº ³ð" << endl;
+	}
+}
